e02f7552bf1a7c0609361665b513d45bfcca5646
James Iry <jamesiry@gmail.com>
2013-01-07 13:01:28 -0800

    add mean and standard deviation calculation.

--- src/main/scala/scala/collection/mutable/Benchmark.scala
+++ src/main/scala/scala/collection/mutable/Benchmark.scala
@@ -3,18 +3,32 @@ package scala.collection.mutable
 import com.gs.collections.impl.map.mutable.UnifiedMap
 
 object Benchmark extends App {
+  // number of times the whole thing is repeated
+  val cycles = 100
+  // number of times the results are ignored to allow the JVM to warm up
+  val ignoredCycles = 3
+  // the chance of a get succeeding
+  val hitRate = .9
+  // start size of the hash map
+  val startSize = 1
+  // ending size of the hash map
+  val endSize = 1000000
+  // the number of key/value pairs inserted per timing
+  // so if the current size is 10, then we'll do 100000 repeats of creating
+  // a hash map and inserting 10 elements
+  val entriesPerTest = 1000000
 
-  test(List(ScalaHashMapWrapper, UnifiedMapWrapper, CompactHashMapWrapper))
+  test()
 
-  def test(wrappers: List[Wrapper]) = {
-    val entriesPerTest = 1000000
-    val hitRate = .9
-    val startSize = 1
-    val endSize = 1000000
+  // this is made a method just because not doing so gives a warning about the foreach not being inlined
+  def test() = {
+    val wrappers = List(ScalaHashMapWrapper, UnifiedMapWrapper, CompactHashMapWrapper)
+    val results = new LinkedHashMap[String, List[Double]]
     val r = new scala.util.Random
-    val cycles = 10
 
     0 until cycles foreach { cycle =>
+      println(s"##### cycle ${cycle + 1} of ${cycles} ${if (cycle < ignoredCycles) "will be ignored" else ""} ####")
+      println
       var size = startSize
       while (size <= endSize) {
         val universeSize: Int = (size / hitRate).toInt
@@ -22,7 +36,8 @@ object Benchmark extends App {
         val values = (0 until universeSize map { x => r.nextInt.toString }).toArray
 
         wrappers foreach { wrapper =>
-          time(s"${"%4d".format(cycle)}, ${wrapper.testType}, ${"%7d".format(size)} entries") {
+          val msg = s"${wrapper.testType}, ${"%7d".format(size)} entries"
+          val (result, micros) = time(msg) {
             val repeats = entriesPerTest / size
             0 until repeats foreach { repeat =>
               wrapper.init
@@ -37,6 +52,11 @@ object Benchmark extends App {
               }
             }
           }
+
+          // ignore the first cycle because that's JVM warm up time
+          if (cycle >= ignoredCycles)
+            results.put(msg, micros.toDouble :: (results.get(msg) getOrElse Nil))
+
           System.gc()
         }
         println
@@ -44,6 +64,9 @@ object Benchmark extends App {
         size *= 10
       }
     }
+    println("#### overall ####")
+    println
+    for ((msg, times) <- results) println(s"${msg}, mean ${"%9d" format mean(times).toLong} microseconds (stddev ${stddev(times).toLong})")
   }
 
   def time[A](msg: String)(a: => A) = {
@@ -51,7 +74,7 @@ object Benchmark extends App {
     val result = a
     val micros = (System.nanoTime - now) / 1000
     println(s"${msg}, ${"%9d".format(micros)} microseconds")
-    result
+    (result, micros)
   }
 
   abstract class Wrapper {
@@ -97,4 +120,15 @@ object Benchmark extends App {
       case x => Some(x)
     }
   }
+
+  def mean(items: List[Double]) = items.sum / items.size.toDouble
+
+  def variance(items: List[Double]) = {
+    val itemMean = mean(items)
+    val sumOfSquares = (items map { x => math.pow(x - itemMean, 2) }).sum
+    sumOfSquares / (items.size.toDouble - 1)
+  }
+
+  def stddev(items: List[Double]) = math.sqrt(variance(items))
+
 }
