6431fa796aa4aba4a4ee0b689d1ef4a173a32d2a
James Iry <jamesiry@gmail.com>
2013-01-14 10:25:39 -0800

    Change benchmark to use Caliper.

---
+++ project/Build.scala
@@ -0,0 +1,33 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+
+  lazy val project = Project("root", file(".")).settings(
+
+    // basics
+    name := "CompactHashTable",
+    organization := "com.typesafe",
+    version := "1.0.0-SNAPSHOT",
+    scalaVersion := "2.10.0",
+
+    scalacOptions ++= Seq("-deprecation", "-feature", "-optimise", "-Yinline-warnings"),
+
+    // dependencies
+    libraryDependencies ++= Seq(
+        "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
+        "com.google.code.gson" % "gson" % "1.7.1",
+        "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP5",
+        // only needed by gs-collections which is used for benchmarking
+        "net.jcip" % "jcip-annotations" % "1.0"
+    ),
+    resolvers += "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+
+    // enable forking in run
+    fork in run := true,
+
+    // we need to add the runtime classpath as a "-cp" argument to the `javaOptions in run`, otherwise caliper
+    // will not see the right classpath and die with a ConfigurationException
+    javaOptions in run <++= (fullClasspath in Runtime) map { cp => Seq("-cp", sbt.Build.data(cp).mkString(":")) }
+  )
+}
--- src/main/scala/scala/collection/mutable/Benchmark.scala
+++ src/main/scala/scala/collection/mutable/Benchmark.scala
@@ -1,134 +1,134 @@
 package scala.collection.mutable
 
+import annotation.tailrec
+import collection.JavaConverters._
+import util.Random
+
 import com.gs.collections.impl.map.mutable.UnifiedMap
 
-object Benchmark extends App {
-  // number of times the whole thing is repeated
-  val cycles = 20
-  // number of times the results are ignored to allow the JVM to warm up
-  val ignoredCycles = cycles / 10
-  // the chance of a get succeeding
-  val hitRate = .9
-  // start size of the hash map
-  val startSize = 1
-  // ending size of the hash map
-  val endSize = 1000000
-  // the number of key/value pairs inserted per timing
-  // so if the current size is 10, then we'll do 100000 repeats of creating
-  // a hash map and inserting 10 elements
-  val entriesPerTest = endSize
+import com.google.caliper.{Runner => CaliperRunner, Param, SimpleBenchmark}
 
-  test()
 
-  // this is made a method just because not doing so gives a warning about the foreach not being inlined
-  def test() = {
-    val wrappers = List(ScalaHashMapWrapper, UnifiedMapWrapper, CompactHashMapWrapper)
-    val results = new LinkedHashMap[String, List[Double]]
-    val r = new scala.util.Random
+object Benchmark {
+  def main(args: Array[String]) {
+    // we simply pass in the CLI args,
+    // we could of course also just pass hardcoded arguments to the caliper Runner
+    val newargs = args ++ Array("-Jmemory=-Xmx1024m"/*, "--warmupMillis", "10000"*/)
+    CaliperRunner.main(classOf[Benchmark], newargs: _*)
+  }
 
-    0 until cycles foreach { cycle =>
-      println(s"##### cycle ${cycle + 1} of ${cycles} ${if (cycle < ignoredCycles) "will be ignored" else ""} ####")
-      println
-      var size = startSize
-      while (size <= endSize) {
-        val universeSize: Int = (size / hitRate).toInt
-        val keys = (0 until universeSize map { x => r.nextInt.toString }).toArray
-        val values = (0 until universeSize map { x => r.nextInt.toString }).toArray
+  def mapTypeValues = List(ScalaHashMapWrapper, UnifiedMapWrapper, CompactHashMapWrapper).asJava
 
-        wrappers foreach { wrapper =>
-          val msg = s"${wrapper.testType}, ${"%7d".format(size)} entries"
-          val (result, micros) = time(msg) {
-            val repeats = entriesPerTest / size
-            0 until repeats foreach { repeat =>
-              wrapper.init
-              0 until size foreach { i =>
-                wrapper.put(keys(i), values(i))
-              }
-              0 until universeSize foreach { i =>
-                wrapper.get(keys(i))
-              }
-              0 until universeSize foreach { i =>
-                wrapper.remove(keys(i))
-              }
-            }
-          }
-
-          // ignore the first cycle because that's JVM warm up time
-          if (cycle >= ignoredCycles)
-            results.put(msg, micros.toDouble :: (results.get(msg) getOrElse Nil))
-
-          System.gc()
-        }
-        println
-
-        size *= 10
-      }
+  def repeat[@specialized A](reps: Int)(snippet: => A) = {
+    val zero = 0.asInstanceOf[A] // looks weird but does what it should: init w/ default value in a fully generic way
+    var i = 0
+    var result = zero
+    while (i < reps) {
+      val res = snippet 
+      if (res != zero) result = res // make result depend on the benchmarking snippet result 
+      i = i + 1
     }
-    println("#### overall ####")
-    println
-    for ((msg, times) <- results) println(s"${msg}, mean ${"%9d" format mean(times).toLong} microseconds (stddev ${stddev(times).toLong})")
+    result
   }
-
-  def time[A](msg: String)(a: => A) = {
-    val now = System.nanoTime
-    val result = a
-    val micros = (System.nanoTime - now) / 1000
-    println(s"${msg}, ${"%9d".format(micros)} microseconds")
-    (result, micros)
-  }
-
-  abstract class Wrapper {
-    def testType: String
-    def init: Unit
-    def put(key: String, value: String): Option[String]
-    def get(key: String): Option[String]
-    def remove(key: String): Option[String]
-  }
-
-  object ScalaHashMapWrapper extends Wrapper {
-    def testType = "Scala HashMap "
-    private var hm: HashMap[String, String] = _
-    def init = hm = new HashMap[String, String]
-    def put(key: String, value: String) = hm.put(key, value)
-    def get(key: String) = hm.get(key)
-    def remove(key: String) = hm.remove(key)
-  }
-
-  object CompactHashMapWrapper extends Wrapper {
-    var hm: CompactHashMap[String, String] = _
-    def testType = "CompactHashMap"
-    def init = hm = CompactHashMap[String, String]
-    def put(key: String, value: String) = hm.put(key, value)
-    def get(key: String) = hm.get(key)
-    def remove(key: String) = hm.remove(key)
-  }
-
-  object UnifiedMapWrapper extends Wrapper {
-    var hm: UnifiedMap[String, String] = _
-    def testType = "GS UnifiedMap "
-    def init = hm = new UnifiedMap[String, String]()
-    def put(key: String, value: String) = hm.put(key, value) match {
-      case null => None
-      case x => Some(x)
-    }
-    def get(key: String) = hm.get(key) match {
-      case null => None
-      case x => Some(x)
-    }
-    def remove(key: String) = hm.remove(key) match {
-      case null => None
-      case x => Some(x)
-    }
-  }
-
-  def mean(items: List[Double]) = items.sum / items.size.toDouble
-
-  def variance(items: List[Double]) = {
-    val itemMean = mean(items)
-    val sumOfSquares = (items map { x => math.pow(x - itemMean, 2) }).sum
-    sumOfSquares / (items.size.toDouble - 1)
-  }
-
-  def stddev(items: List[Double]) = math.sqrt(variance(items))
-
 }
+
+class Benchmark extends SimpleBenchmark {
+  import Benchmark._
+
+  @Param
+  var mapType : Wrapper = _
+
+  @Param(Array("1", "10", "100", "1000", "10000", "100000", "1000000"))
+  var size: Int = _
+
+  var universeSize: Int = _
+  var keys : Array[String] = _
+  var values : Array[String] = _
+
+  val hitRate = .9
+
+  override def setUp() {
+    val r = Random
+    universeSize = (size / hitRate).toInt
+    keys = (0 until universeSize map { x => r.nextInt.toString }).toArray
+    values = (0 until universeSize map { x => r.nextInt.toString }).toArray
+  }
+ 
+  def timeAll(reps : Int) = {
+    repeat(reps){
+      mapType.init
+      var result = 0
+      0 until size foreach { i =>
+        result += mapType.put(keys(i), values(i)).hashCode
+      }
+      0 until universeSize foreach { i =>
+        result += mapType.get(keys(i)).hashCode
+      }
+      0 until universeSize foreach { i =>
+        result += mapType.remove(keys(i)).hashCode
+      }
+      result
+    }
+  }
+}
+
+object Wrapper {
+  val ScalaHashMapName = "ScalaHashMap"
+  val CompactHashMapName = "CompactHashMap" 
+  val UnifiedMapName = "GSUnifiedMap" 
+
+  def valueOf(name : String) = name match {
+    case ScalaHashMapName => ScalaHashMapWrapper
+    case CompactHashMapName => CompactHashMapWrapper
+    case UnifiedMapName => UnifiedMapWrapper
+  }
+}  
+
+abstract class Wrapper {
+  import Wrapper._
+
+  override def toString = testType
+  def testType: String
+  def init: Unit
+  def put(key: String, value: String): Option[String]
+  def get(key: String): Option[String]
+  def remove(key: String): Option[String]
+}
+
+object ScalaHashMapWrapper extends Wrapper {
+  def testType = Wrapper.ScalaHashMapName
+  private var hm: HashMap[String, String] = _
+  def init = hm = new HashMap[String, String]
+  def put(key: String, value: String) = hm.put(key, value)
+  def get(key: String) = hm.get(key)
+  def remove(key: String) = hm.remove(key)
+}
+
+object CompactHashMapWrapper extends Wrapper {
+  var hm: CompactHashMap[String, String] = _
+  def testType = Wrapper.CompactHashMapName
+  def init = hm = CompactHashMap[String, String]
+  def put(key: String, value: String) = hm.put(key, value)
+  def get(key: String) = hm.get(key)
+  def remove(key: String) = hm.remove(key)
+}
+
+object UnifiedMapWrapper extends Wrapper {
+  var hm: UnifiedMap[String, String] = _
+  def testType = Wrapper.UnifiedMapName
+  def init = hm = new UnifiedMap[String, String]()
+  def put(key: String, value: String) = hm.put(key, value) match {
+    case null => None
+    case x => Some(x)
+  }
+  def get(key: String) = hm.get(key) match {
+    case null => None
+    case x => Some(x)
+  }
+  def remove(key: String) = hm.remove(key) match {
+    case null => None
+    case x => Some(x)
+  }
+}
+
+
